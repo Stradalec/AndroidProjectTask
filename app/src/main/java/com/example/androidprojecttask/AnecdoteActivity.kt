1a3ef893b653b6d2960eff383888677877129008
Stradalec <pochtatipo6@gmail.com>
2024-12-10 22:15:26 +0700

    some extra silly changes

--- app/src/main/java/com/example/androidprojecttask/AnecdoteActivity.kt
+++ app/src/main/java/com/example/androidprojecttask/AnecdoteActivity.kt
@@ -1,6 +1,7 @@
 package com.example.androidprojecttask
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,5 +17,15 @@ class AnecdoteActivity : AppCompatActivity() {
         val text = intent.getStringExtra("anecdote_text")
         val textView = findViewById<TextView>(R.id.anecdote_text)
         textView.text = text
+        val buttonFavorite = findViewById<Button>(R.id.button_favorite)
+        buttonFavorite.setOnClickListener {
+            val anecdoteText = text
+            val sharedPreferences = getSharedPreferences("anecdotes", MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putString(anecdoteText, anecdoteText)
+            editor.apply()
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/androidprojecttask/MainActivity.kt
+++ app/src/main/java/com/example/androidprojecttask/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidprojecttask
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
@@ -27,6 +28,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val buttonParse = findViewById<Button>(R.id.button_parse)
+        val buttonFavorites = findViewById<Button>(R.id.button_favorites)
+        buttonFavorites.setOnClickListener {
+            val intent = Intent(this, FavoritesActivity::class.java)
+            startActivity(intent)
+        }
+
         recyclerView = findViewById(R.id.rview)
         recyclerView.layoutManager = LinearLayoutManager(this)
         adapter = AnecdoteAdapter(mutableListOf())
@@ -38,30 +46,33 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         })
+        buttonParse.setOnClickListener() {
+            lifecycleScope.launch {
+                val listOfAnecdotes = mutableListOf<Anecdote>()
+                for (index in 1..10) {
+                    val randomNumber = (1..1142).random()
+                    val response = fetchAnecdote(randomNumber)
+                    if (response.isSuccessful) {
+                        val html = response.body?.string()
+                        val doc = Jsoup.parse(html ?: "")
+                        val article = doc.select("article")
+                        val p = article.select("p")
+                        val anecdoteText = p.text()
+                        val parsedAnecdote = Anecdote(randomNumber, anecdoteText)
+                        listOfAnecdotes.add(parsedAnecdote)
+                    } else {
 
-
-        lifecycleScope.launch {
-            val listOfAnecdotes = mutableListOf<Anecdote>()
-            for (index in 1..10) {
-                val randomNumber = (1..1142).random()
-                val response = fetchAnecdote(randomNumber)
-                if (response.isSuccessful) {
-                    val html = response.body?.string()
-                    val doc = Jsoup.parse(html ?: "")
-                    val article = doc.select("article")
-                    val p = article.select("p")
-                    val anecdoteText = p.text()
-                    val parsedAnecdote = Anecdote(randomNumber, anecdoteText)
-                    listOfAnecdotes.add(parsedAnecdote)
-                } else {
-
+                    }
+                }
+                withContext(Dispatchers.Main) {
+                    adapter.updateAnecdotes(listOfAnecdotes)
+                    adapter.notifyDataSetChanged()
                 }
             }
-            withContext(Dispatchers.Main) {
-                adapter.updateAnecdotes(listOfAnecdotes)
-                adapter.notifyDataSetChanged()
-            }
         }
+
+
+
     }
 
     private suspend fun fetchAnecdote(randomNumber: Int): Response {
